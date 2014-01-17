550c24d8589539d6dd45e1db01b7c4fcaa286fd8
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-01-17 14:21:11 +0100

    Paid out (Docdata payout exports) versus received (bank statements) funds

---
+++ apps/accounting/queries/docdata_bank_match.sql
@@ -0,0 +1,34 @@
+-- Paid out (Docdata payout exports) versus received (bank statements) funds
+
+WITH docdata_bank_in AS (
+  -- Incoming money from Docdata
+  SELECT
+    date_trunc('month', book_date)::date AS month,
+    SUM(amount) AS amount
+  FROM
+    public.accounting_banktransaction
+  WHERE
+    credit_debit = 'C' AND
+    counter_name ILIKE '%docdata%'
+  GROUP BY month
+  ORDER BY month
+), docdata_import_out AS (
+  -- Money going out from Docdata, should match incoming money
+  SELECT
+    date_trunc('month', end_date)::date AS month,
+    SUM(total) AS amount
+  FROM
+    public.accounting_docdatapayout
+  GROUP BY month
+  ORDER BY month
+)
+
+SELECT
+  docdata_import_out.month,
+  docdata_import_out.amount AS amount_out,
+  docdata_bank_in.amount AS amount_in,
+  docdata_import_out.amount - docdata_bank_in.amount AS difference,
+  SUM(docdata_import_out.amount - docdata_bank_in.amount) OVER(ORDER BY docdata_import_out.month) as running_difference
+FROM docdata_import_out
+LEFT JOIN
+  docdata_bank_in ON docdata_bank_in.month = docdata_import_out.month

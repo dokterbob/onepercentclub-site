d562bbe0621609f51c85dff86e58efd538153e76
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-01-17 16:06:15 +0100

    Cleanup, description of fields for queries.

--- apps/accounting/queries/donations_payouts_per_month.sql
+++ apps/accounting/queries/donations_payouts_per_month.sql
@@ -1,4 +1,18 @@
 ﻿-- Monthly aggregate of donations versus payouts
+
+-- List of months, pending/paid donations and completed payouts over 2014.
+
+-- Fields:
+-- - month: month a donation was 'ready'/closed / payout was set to completed
+-- - donation_count: total number of paid/pending donations
+-- - donations: total amount of donations received (paid/pending)
+-- - payouts_count: amount of payouts in the month
+-- - payouts_amount_raised: amount of donations when the payout was set in progress (bank transfer initiated)
+-- - payouts_organization_fee: organization fee calculated from payouts_amount_raised according to payout_rule
+-- - payouts_amount_paid: payable amount after substracting organization_fee from amount_raised
+-- - amount_payable: donations - amount_raised
+-- - running_amount_payable: difference of amount_payable running over months
+
 WITH payment_methods AS (
     SELECT DISTINCT
       order_id, payment_method_id AS payment_method
@@ -27,7 +41,7 @@ WITH payment_methods AS (
       SUM(amount_payable) AS amount_payable
     FROM
       payouts_payout AS payouts
-    WHERE status IN ('completed')
+    WHERE status = 'completed'
     GROUP BY month
     ORDER BY month
 )
@@ -35,7 +49,7 @@ SELECT
   donations.month,
   donations.count AS donation_count,
   donations.amount AS donations_amount,
-  payouts.count AS payouts_count,
+  payouts.count AS payouts_amount_count,
   payouts.amount_raised AS payouts_amount_raised,
   payouts.organization_fee AS payouts_organization_fee,
   payouts.amount_payable AS payouts_amount_paid,
--- apps/accounting/queries/donations_payouts_per_project.sql
+++ apps/accounting/queries/donations_payouts_per_project.sql
@@ -1,4 +1,20 @@
 -- Per project aggregate of donations versus payouts
+
+-- List of projects, pending/paid donations and completed payouts in 2014.
+
+-- Fields:
+-- - id: project id
+-- - title: project title
+-- - last_donation: 'ready'/closed date of last paid/pending donation
+-- - donation_count: total number of paid/pending donations
+-- - donations: total amount of donations received (paid/pending)
+-- - payout_date: date associated payout was completed
+-- - payout_rule: payout rule used for calculating fees
+-- - payouts_amount_raised: amount of donations when the payout was set in progress (bank transfer initiated)
+-- - payouts_organization_fee: organization fee calculated from payouts_amount_raised according to payout_rule
+-- - payouts_amount_paid: payable amount after substracting organization_fee from amount_raised
+-- - amount_payable: donations - amount_raised
+
 WITH payment_methods AS (
     SELECT DISTINCT
       order_id, payment_method_id AS payment_method
@@ -20,19 +36,6 @@ WITH payment_methods AS (
       donations.ready BETWEEN '2013-01-01' AND '2014-01-01'
     GROUP BY project_id
     ORDER BY project_id
-), payouts AS (
-    SELECT
-      project_id,
-      MAX(completed::date) AS completed,
-      COUNT(id),
-      SUM(amount_raised) AS amount_raised,
-      SUM(organization_fee) AS organization_fee,
-      SUM(amount_payable) AS amount_payable
-    FROM
-      payouts_payout AS payouts
-    WHERE status IN ('progress', 'completed')
-    GROUP BY project_id
-    ORDER BY project_id
 )
 SELECT
   projects.id,
@@ -40,15 +43,16 @@ SELECT
   donations.last_donation,
   donations.count AS donation_count,
   donations.amount AS donations,
-  payouts.count AS payouts_count,
   payouts.completed AS payout_date,
+  payouts.payout_rule AS payout_rule,
   payouts.amount_raised AS payouts_amount_raised,
   payouts.organization_fee AS payouts_organization_fee,
   payouts.amount_payable AS payouts_amount_paid,
   donations.amount - coalesce(payouts.amount_raised, 0.0) AS amount_payable
 FROM
   donations
-LEFT JOIN payouts
-  ON payouts.project_id = donations.project_id
+LEFT JOIN payouts_payout AS payouts
+  ON payouts.project_id = donations.project_id AND
+     payouts.status = 'completed'
 JOIN projects_project AS projects
   ON donations.project_id = projects.id
